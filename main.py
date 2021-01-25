2a10cee8ccef95f5bff47cd94d9028df790fb821
root <root@localhost.localdomain>
2021-01-24 23:53:31 -0500

    update for time

--- main.py
+++ main.py
@@ -31,33 +31,20 @@ def a(end, s000, u, l, since_id=""):
         if not 'mblog' in i:
             continue
         k = i['mblog']
-        d465 = str(tran(k['created_at']))
-        s = int(time.mktime(time.strptime(d465, "%Y-%m-%d")))
+        d465 = str(k['created_at'])
+        s = int(time.mktime(time.strptime(d465, "%a %b %d %H:%M:%S %z %Y")))
         if s < e:
             break
         elif s > s001:
             continue
         v1, v2, v3, v4 = int(k['comments_count']), int(k['attitudes_count']), int(k['pending_approval_count']), int(
             k['reposts_count'])
-        l.append((v1, v2, v3, v4, tran(k['created_at']), True))
+        l.append((v1, v2, v3, v4, k['created_at'], True))
         sv1, sv2, sv3, sv4 = sv1 + v1, sv2 + v2, sv3 + v3, sv4 + v4
     if s >= e:
         rk, (_a, _b, _c, _d) = a(end, s000, u, l, since_id)
         sv1, sv2, sv3, sv4 = sv1 + _a, sv2 + _b, sv3 + _c, sv4 + _d
     return l, (sv1, sv2, sv3, sv4)
-
-
-def tran(t):
-    if "小时前" in t:
-        return datetime.date.today()
-    elif "昨天" in t:
-        return datetime.date.today() + datetime.timedelta(-1)
-    elif "分钟前" in t:
-        return datetime.date.today()
-    elif "-" in t:
-        return "2020-" + t
-    else:
-        return t
 
 
 def get_data_and_write(start, end, sour):
